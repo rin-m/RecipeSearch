c563c7e5bc4a1306d93ccbc6feabad483e4da248
rin-m <s2010048@klis.tsukuba.ac.jp>
2023-09-02 23:45:59 +0900

    ユークリッド距離を計算する部分のリファクタリング

--- app.py
+++ app.py
@@ -73,15 +73,21 @@ def recommended_recipe_list():
     # データベースからデータを取得
     recipe_list = mood_sql()
 
+    # レシピデータの気分と分類を取得し標準化を行う
+    recipe_list_std, mood_values = preprocess_recipe_list(recipe_list)
+
     # POSTメソッドから取得した気分の値とレシピデータの気分の値のユークリッド距離を算出
-    euclidean_distance_list = euclidean_distance(recipe_list)
+    euclidean_distance_list = calculate_euclidean_distance(recipe_list_std, mood_values)
+
+    # POSTメソッドから取得した気分の値とレシピデータの気分の値のユークリッド距離を算出
+    # euclidean_distance_list = euclidean_distance(recipe_list)
 
     # ユークリッド距離の値のリストとレシピデータのリストを結合
-    recommended_recipes = sort_by_euclidean_distance(recipe_list, euclidean_distance_list)
+    recommended_recipes = sort_by_distance(recipe_list, euclidean_distance_list)
 
     return recommended_recipes
 
-
+'''
 def euclidean_distance(recipe_list):
 
     # レシピデータの気分と分類を取得
@@ -140,17 +146,79 @@ def euclidean_distance(recipe_list):
     euclidean_distance_list = np.sqrt(list(squared_diff_columns_sum))
 
     return euclidean_distance_list
+'''
+    
+
+def preprocess_recipe_list(recipe_list):
+    # レシピデータの気分のラベルを取得['mood', 'body', 'money', 'time']
+    moods = recipe_list[0][4:8]
+
+    # レシピデータの気分と分類を取得
+    recipe_list_syusyoku = []
+    recipe_list_syusai = []
+    recipe_list_hukusai = []
+    recipe_list_mood = []
+    recipe_list_body = []
+    recipe_list_money = []
+    recipe_list_time = []
+
+    # レシピデータの気分と分類をリストに格納(先頭行のラベルを除く)
+    for row in recipe_list[1:]:
+        recipe_list_syusyoku.append(int(list(row)[1]))  # 「主食」の行のみを抽出
+        recipe_list_syusai.append(int(list(row)[2]))    # 「主菜」の行のみを抽出
+        recipe_list_hukusai.append(int(list(row)[3]))   # 「副菜」の行のみを抽出
+        recipe_list_mood.append(int(list(row)[4]))      # 「精神」の行のみを抽出
+        recipe_list_body.append(int(list(row)[5]))      # 「身体」の行のみを抽出
+        recipe_list_money.append(int(list(row)[6]))     # 「経済」の行のみを抽出
+        recipe_list_time.append(int(list(row)[7]))      # 「時間」の行のみを抽出
+
+    # レシピデータの気分の値を標準化
+    recipe_list_mood_std = preprocessing.scale(recipe_list_mood)
+    recipe_list_body_std = preprocessing.scale(recipe_list_body)
+    recipe_list_money_std = preprocessing.scale(recipe_list_money)
+    recipe_list_time_std = preprocessing.scale(recipe_list_time)
+
+    # レシピデータの気分の値を標準化した値をリストに格納
+    recipe_moods_std = [recipe_list_mood_std, recipe_list_body_std, recipe_list_money_std, recipe_list_time_std]
+
+    # POSTメソッドから取得した気分の値を取得
+    mood_values = [float(request.form[mood]) for mood in moods]
+
+    return recipe_moods_std, mood_values
 
 
-def sort_by_euclidean_distance(recipe_list, euclidean_distance_list):
-    # ユークリッド距離の値のリストとデータベースから取得したデータのリストを結合
+def calculate_euclidean_distance(recipe_moods_std, mood_values):
+    squared_diff = []
+
+    for mood_index, mood_value in enumerate(mood_values):
+        if mood_value != 0:
+
+            diff_mood = recipe_moods_std[mood_index] - mood_value # レシピデータの気分とPOSTメソッドから取得したmoodの値の差を計算
+            squared_diff_mood = np.square(diff_mood)                 # 差の2乗を計算
+        else:
+            # スライダーが0を選択しているときはユークリッド距離を計算しない
+            squared_diff_mood = np.zeros(len(recipe_moods_std), dtype=float)
+        
+        squared_diff.append(squared_diff_mood)
+    
+    # 各列の合計を求める
+    squared_diff_columns_sum = np.sum(np.array(squared_diff), axis=0)
+
+    # 各要素の平方根を求める
+    euclidean_distance_list = np.sqrt(squared_diff_columns_sum)
+
+    return euclidean_distance_list
+
+
+def sort_by_distance(recipe_list, euclidean_distance_list):
+    # 距離の値のリストとデータベースから取得したデータのリストを結合
     combined_list = []
     recipe_list_deleted_label = recipe_list[1:] # 先頭行のラベルを除く
     for row_num in range(len(recipe_list_deleted_label)):
         recipe_list_deleted_label[row_num].append(euclidean_distance_list[row_num])
         combined_list.append(recipe_list_deleted_label[row_num])
 
-    # ユークリッド距離に基づいてリストをソート
+    # 距離に基づいてリストをソート(昇順)
     sorted_list = sorted(combined_list, key=lambda x: x[8])
 
     # 多様性検索の機能
