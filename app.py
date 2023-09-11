07835321e2ac04c5cd4a8c7c519760f10c86e41a
rin-m <s2010048@klis.tsukuba.ac.jp>
2023-09-11 22:16:54 +0900

    時間の値だけを標準化

--- app.py
+++ app.py
@@ -89,22 +89,19 @@ def preprocess_recipe_list(recipe_list):
 
     # レシピデータの気分と分類をリストに格納(先頭行のラベルを除く)
     for row in recipe_list[1:]:
-        recipe_list_syusyoku.append(int(list(row)[1]))  # 「主食」の行のみを抽出
-        recipe_list_syusai.append(int(list(row)[2]))    # 「主菜」の行のみを抽出
-        recipe_list_hukusai.append(int(list(row)[3]))   # 「副菜」の行のみを抽出
-        recipe_list_mood.append(int(list(row)[4]))      # 「精神」の行のみを抽出
-        recipe_list_body.append(int(list(row)[5]))      # 「身体」の行のみを抽出
-        recipe_list_money.append(int(list(row)[6]))     # 「経済」の行のみを抽出
-        recipe_list_time.append(int(list(row)[7]))      # 「時間」の行のみを抽出
+        recipe_list_syusyoku.append(float(list(row)[1]))  # 「主食」の行のみを抽出
+        recipe_list_syusai.append(float(list(row)[2]))    # 「主菜」の行のみを抽出
+        recipe_list_hukusai.append(float(list(row)[3]))   # 「副菜」の行のみを抽出
+        recipe_list_mood.append(float(list(row)[4]))      # 「精神」の行のみを抽出
+        recipe_list_body.append(float(list(row)[5]))      # 「身体」の行のみを抽出
+        recipe_list_money.append(float(list(row)[6]))     # 「経済」の行のみを抽出
+        recipe_list_time.append(float(list(row)[7]))      # 「時間」の行のみを抽出
 
-    # レシピデータの気分の値を標準化
-    recipe_list_mood_std = preprocessing.scale(recipe_list_mood)
-    recipe_list_body_std = preprocessing.scale(recipe_list_body)
-    recipe_list_money_std = preprocessing.scale(recipe_list_money)
+    # レシピデータの時間の値を標準化
     recipe_list_time_std = preprocessing.scale(recipe_list_time)
 
     # レシピデータの気分の値を標準化した値をリストに格納
-    recipe_moods_std = [recipe_list_mood_std, recipe_list_body_std, recipe_list_money_std, recipe_list_time_std]
+    recipe_moods_std = [recipe_list_mood, recipe_list_body, recipe_list_money, recipe_list_time_std]
 
     # POSTメソッドから取得した気分の値を取得
     mood_values = [float(request.form[mood]) for mood in moods]
@@ -112,22 +109,22 @@ def preprocess_recipe_list(recipe_list):
     return recipe_moods_std, mood_values
 
 
-def calculate_euclidean_distance(recipe_moods_std, mood_values):
+def calculate_euclidean_distance(recipe_moods, mood_values):
     squared_diff = []
 
     for mood_index, mood_value in enumerate(mood_values):
         if mood_value != 0:
 
-            diff_mood = recipe_moods_std[mood_index] - mood_value # レシピデータの気分とPOSTメソッドから取得したmoodの値の差を計算
+            diff_mood = np.array(recipe_moods[mood_index]) - mood_value # レシピデータの気分とPOSTメソッドから取得したmoodの値の差を計算
             squared_diff_mood = np.square(diff_mood)                 # 差の2乗を計算
         else:
             # スライダーが0を選択しているときはユークリッド距離を計算しない
-            squared_diff_mood = np.zeros(len(recipe_moods_std[0]), dtype=float)
+            squared_diff_mood = np.zeros(len(recipe_moods[0]), dtype=float)
         
         squared_diff.append(squared_diff_mood)
     
     # 各列の合計を求める
-    squared_diff_columns_sum = np.sum(np.array(squared_diff), axis=0)
+    squared_diff_columns_sum = np.sum(squared_diff, axis=0)
 
     # 各要素の平方根を求める
     euclidean_distance_list = np.sqrt(squared_diff_columns_sum)
