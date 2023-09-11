41617efb964285df7de9b0214e8ce08772d20ef2
rin-m <s2010048@klis.tsukuba.ac.jp>
2023-09-11 10:59:35 +0900

    コメントアウトされたプログラムを削除

--- app.py
+++ app.py
@@ -11,37 +11,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-'''
-# キーワード検索
-@app.route("/sample1", methods = ["POST"])
-def sample1():
-    k = keyword_sql()
-    con = sqlite3.connect("recipe.db")
-    cur = con.cursor()
-    s = "<!DOCTYPE html>\n"
-    s += '<html><head><meta charset="UTF-8"><title>検索結果</title></head>\n<body>'
-    s += '<div><h1>検索結果</h1></div>\n'
-    for row in cur.execute("{}".format(k)):
-        s += "<div><p>{}<hr></p></div>".format(escape(row[0]))
-    con.close()
-    s += "</body></html>"
-    return s
-
-
-#区切り文字で検索キーワードを分割してsql文を生成
-def keyword_sql():
-    l = request.form["keyword"]
-    if l != "":
-        m = l.split()
-        n = "select * from recipe where "
-        for o in m:
-            n += "(name like '%{}%') and ".format(o,o)
-        n = n.rstrip(' and ')
-        n += ';'
-    else:
-        n = "select * from recipe;"
-    return n
-'''
 
 @app.route("/mood1", methods = ["POST"])
 def mood1():
@@ -86,67 +55,6 @@ def recommended_recipe_list():
     recommended_recipes, sorted_list = sort_by_distance(recipe_list, euclidean_distance_list)
 
     return recommended_recipes, sorted_list
-
-'''
-def euclidean_distance(recipe_list):
-
-    # レシピデータの気分と分類を取得
-    recipe_list_syusyoku = []
-    recipe_list_syusai = []
-    recipe_list_hukusai = []
-    recipe_list_mood = []
-    recipe_list_body = []
-    recipe_list_money = []
-    recipe_list_time = []
-
-    # レシピデータの気分と分類をリストに格納(先頭行のラベルを除く)
-    for row in recipe_list[1:]:
-        recipe_list_syusyoku.append(int(list(row)[1]))  # 「主食」の行のみを抽出
-        recipe_list_syusai.append(int(list(row)[2]))    # 「主菜」の行のみを抽出
-        recipe_list_hukusai.append(int(list(row)[3]))   # 「副菜」の行のみを抽出
-        recipe_list_mood.append(int(list(row)[4]))      # 「精神」の行のみを抽出
-        recipe_list_body.append(int(list(row)[5]))      # 「身体」の行のみを抽出
-        recipe_list_money.append(int(list(row)[6]))     # 「経済」の行のみを抽出
-        recipe_list_time.append(int(list(row)[7]))      # 「時間」の行のみを抽出
-
-    # POSTメソッドから取得したmoodの値(-5~5)をリストに保存
-    squared_diff = []
-    moods = recipe_list[0][4:8] # レシピデータの気分のラベルを取得['mood', 'body', 'money', 'time']
-
-    # レシピデータの気分の値を標準化
-    recipe_list_mood_std = preprocessing.scale(recipe_list_mood)
-    recipe_list_body_std = preprocessing.scale(recipe_list_body)
-    recipe_list_money_std = preprocessing.scale(recipe_list_money)
-    recipe_list_time_std = preprocessing.scale(recipe_list_time)
-
-    squared_diff_moods = [recipe_list_mood_std, recipe_list_body_std, recipe_list_money_std, recipe_list_time_std]
-
-    # レシピデータの気分のラベルの数だけ繰り返す0~3
-    for row in range(len(moods)):
-
-        recipe_list_item = squared_diff_moods[row]
-
-        # POSTメソッドから取得したmoodの値をリストに保存
-        mood_value = [float(request.form[moods[row]])] * len(recipe_list_item)
-
-        if float(request.form[moods[row]]) != 0:
-            recipe_mood_array = np.array(recipe_list_item)
-            mood_array = np.array(mood_value)
-            diff_mood = recipe_mood_array - mood_array  # レシピデータの気分とPOSTメソッドから取得したmoodの値の差を計算
-            squared_diff_mood = np.square(diff_mood)    # 差の2乗を計算
-        else:   # スライダーが0を選択しているときはユークリッド距離を計算しない
-            squared_diff_mood = np.zeros(len(recipe_list_item), int)
-
-        squared_diff.append(squared_diff_mood)
-    
-    # 各列の合計を求める
-    squared_diff_columns_sum = map(sum, zip(*squared_diff))
-
-    # 各要素の平方根を求める
-    euclidean_distance_list = np.sqrt(list(squared_diff_columns_sum))
-
-    return euclidean_distance_list
-'''
     
 
 def preprocess_recipe_list(recipe_list):
